b3604a6bf0f4bb6337120942460edb1f3b9df570
Nayara Fugii <nayarac.neto@gmail.com>
2021-06-17 14:38:45 -0300

    project restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule} from "@angular/platform-browser/animations"
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http"
+
+import { CoreModule} from "./core/core.module"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
-import { InMemoryDatabase} from "./in-memory-database"
 
 
 @NgModule({
@@ -15,11 +11,9 @@ import { InMemoryDatabase} from "./in-memory-database"
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    CoreModule,      
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -127,14 +127,14 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private actionsForError(error){
-    toastr.erros("Ocorreu um erro ao processar a sua solicitação!");
+    toastr.console.error("Ocorreu um erro ao processar a sua solicitação!");
 
     this.submittingForm = false;
 
     if(error.status === 422)
       this.serverErrorMessages = JSON.parse(error._body).errors;
     else
-      this.serverErrorMessages = ["Falha na comunicação com o servidor, Por favor, tente mais tarde."]
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."]
   }
 
 }
