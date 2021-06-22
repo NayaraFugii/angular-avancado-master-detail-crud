e9e05b2208879fdd6dd2e26e59f31135a908cfe0
Nayara Fugii <nayarac.neto@gmail.com>
2021-06-22 16:44:24 -0300

    adding reports base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,10 @@ const routes: Routes = [
   }, 
   {
     path:'categories', loadChildren:'./pages/categories/categories.module#CategoriesModule'
-  }  
+  },
+  {
+    path:'reports', loadChildren:'./pages/reports/reports.module#ReportsModule'
+  }
 ];
 
 @NgModule({
